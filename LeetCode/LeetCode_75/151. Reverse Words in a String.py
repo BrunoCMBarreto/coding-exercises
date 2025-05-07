2ac17d4a02cfdde1515e9b48b7bc00623e30f9c5
brunobarreto <bruno@barreto.us>
2025-05-06 20:45:20 -0700

    Removed additional unused comments from LeetCode75 code

--- LeetCode/LeetCode_75/151. Reverse Words in a String.py
+++ LeetCode/LeetCode_75/151. Reverse Words in a String.py
@@ -12,17 +12,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         return " ".join(s.split()[::-1])
-
-# Claude Response:
-#
-# def reverseWords(s: str) -> str:
-#     # Split the string into words and filter out empty strings
-#     words = [word for word in s.split() if word]
-    
-#     # Reverse the array of words
-#     words.reverse()  # or words = words[::-1]
-    
-#     # Join the words with a single space
-#     return " ".join(words)
-#
-# Due to its use of a list comprehension, it fails the LeetCode tests that have excessively large inputs
