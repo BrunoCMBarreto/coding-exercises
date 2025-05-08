ed52c50a032ce3b5ee8bda300f44efcc95219ad7
brunobarreto <bruno@barreto.us>
2025-05-07 22:01:20 -0700

    Removed additional unused comments from LeetCode75 code

--- LeetCode/LeetCode_75/283. Move Zeroes.py
+++ LeetCode/LeetCode_75/283. Move Zeroes.py
@@ -16,21 +16,3 @@ class Solution:
                 zero_indices.append(ind)
         for iter_ind, zero_ind in enumerate(zero_indices):
             nums.append(nums.pop(zero_ind-iter_ind))
-
-# Clause Response:
-
-# def moveZeroes(nums: list[int]) -> None:
-#     # Position where we will place the next non-zero element
-#     non_zero_pos = 0
-    
-#     # First pass: move all non-zero elements to the front
-#     for i in range(len(nums)):
-#         if nums[i] != 0:
-#             nums[non_zero_pos] = nums[i]
-#             non_zero_pos += 1
-    
-#     # Second pass: fill the remaining positions with zeros
-#     for i in range(non_zero_pos, len(nums)):
-#         nums[i] = 0
-#
-# Runs slower on lists that are not an overwhelming majority zeros
