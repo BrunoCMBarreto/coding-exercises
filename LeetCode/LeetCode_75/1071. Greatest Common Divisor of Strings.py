a5f5df192aa3875e0173313e4b3b593e95705575
brunobarreto <bruno@barreto.us>
2025-05-16 22:14:22 -0700

    Removed unused comments from LeetCode 75 work

--- LeetCode/LeetCode_75/1071. Greatest Common Divisor of Strings.py
+++ LeetCode/LeetCode_75/1071. Greatest Common Divisor of Strings.py
@@ -18,30 +18,3 @@ class Solution:
                 best_divisor = small_str[:chunk_size]
         return best_divisor
     
-# Claude Answer:
-# def gcdOfStrings(str1: str, str2: str) -> str:
-#     """
-#     Find the largest string that divides both str1 and str2.
-    
-#     Args:
-#         str1: First string
-#         str2: Second string
-        
-#     Returns:
-#         The largest string that divides both input strings
-#     """
-#     # If concatenation in different orders doesn't match, no common divisor exists
-#     if str1 + str2 != str2 + str1:
-#         return ""
-    
-#     # Use the GCD algorithm to find the length of the largest common divisor
-#     def gcd(a, b):
-#         while b:
-#             a, b = b, a % b
-#         return a
-    
-#     # GCD of the lengths gives us the length of the largest common divisor
-#     gcd_length = gcd(len(str1), len(str2))
-    
-#     # Return the first gcd_length characters of either string
-#     return str1[:gcd_length]
