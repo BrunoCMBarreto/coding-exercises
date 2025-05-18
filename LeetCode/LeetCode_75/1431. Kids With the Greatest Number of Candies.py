5c48afb24b188ce8d7e5db4b19b04dfde5e1c22f
brunobarreto <bruno@barreto.us>
2025-05-17 21:30:55 -0700

    Removed unused comments from LeetCode 75 work

--- LeetCode/LeetCode_75/1431. Kids With the Greatest Number of Candies.py
+++ LeetCode/LeetCode_75/1431. Kids With the Greatest Number of Candies.py
@@ -14,26 +14,3 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         most_candies = max(candies)
         return [candy_count + extraCandies >= most_candies for candy_count in candies]
-
-
-# Claude Answer:
-#
-# def kidsWithCandies(candies: list[int], extraCandies: int) -> list[bool]:
-#     """
-#     Determine which kids will have the greatest number of candies after being given extraCandies.
-    
-#     Args:
-#         candies: List where candies[i] is the number of candies the ith kid has
-#         extraCandies: Number of extra candies to give to each kid (one at a time)
-        
-#     Returns:
-#         Boolean list where result[i] is True if the ith kid would have the most candies
-#         after receiving extraCandies, otherwise False
-#     """
-#     # Find the maximum number of candies any kid currently has
-#     max_candies = max(candies)
-    
-#     # For each kid, check if their candies + extraCandies will be >= max_candies
-#     result = [candy + extraCandies >= max_candies for candy in candies]
-    
-#     return result
