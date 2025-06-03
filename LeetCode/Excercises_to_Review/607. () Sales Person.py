8f9183eb50ee15f81d9c7e2f8a27e53da5b949c3
brunobarreto <bruno@barreto.us>
2025-06-02 21:39:43 -0700

    Added Pandas question worth reviewing to dedicated folder

---
+++ LeetCode/Excercises_to_Review/607. () Sales Person.py
@@ -0,0 +1,63 @@
+# 607. Sales Person
+# Solved
+# Easy
+# Topics
+# Companies
+# Hint
+# SQL Schema
+# Pandas Schema
+# Table: SalesPerson
+
+# +-----------------+---------+
+# | Column Name     | Type    |
+# +-----------------+---------+
+# | sales_id        | int     |
+# | name            | varchar |
+# | salary          | int     |
+# | commission_rate | int     |
+# | hire_date       | date    |
+# +-----------------+---------+
+# sales_id is the primary key (column with unique values) for this table.
+# Each row of this table indicates the name and the ID of a salesperson alongside their salary, commission rate, and hire date.
+ 
+
+# Table: Company
+
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | com_id      | int     |
+# | name        | varchar |
+# | city        | varchar |
+# +-------------+---------+
+# com_id is the primary key (column with unique values) for this table.
+# Each row of this table indicates the name and the ID of a company and the city in which the company is located.
+ 
+
+# Table: Orders
+
+# +-------------+------+
+# | Column Name | Type |
+# +-------------+------+
+# | order_id    | int  |
+# | order_date  | date |
+# | com_id      | int  |
+# | sales_id    | int  |
+# | amount      | int  |
+# +-------------+------+
+# order_id is the primary key (column with unique values) for this table.
+# com_id is a foreign key (reference column) to com_id from the Company table.
+# sales_id is a foreign key (reference column) to sales_id from the SalesPerson table.
+# Each row of this table contains information about one order. This includes the ID of the company, the ID of the salesperson, the date of the order, and the amount paid.
+ 
+
+# Write a solution to find the names of all the salespersons who did not have any orders related to the company with the name "RED".
+
+# Return the result table in any order.
+
+import pandas as pd
+
+def sales_person(sales_person: pd.DataFrame, company: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    sales_data = pd.merge(pd.merge(orders, company.rename(columns={"name":"company_name"}), on="com_id"), sales_person, how="outer", on="sales_id")
+    return sales_data[~sales_data.name.isin(sales_data[sales_data.company_name == "RED"].name)][["name"]].drop_duplicates(subset="name")
+
